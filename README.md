82c4ee9d06d8c8f614d7462d368b587902fdcffc
vineetpayal <vineet.pyl@gmail.com>
2025-03-08 14:26:00 +0530

    prototype lexical tokenizer

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Genesis
-Setting up project
+
+## Building
+
+Requires `nasm` and `ld` on a Linux operating system.
+
+```bash
+git clone https://github.com/orosmatthew/hydrogen-cpp
+cd genesis-cpp
+mkdir build
+cmake -S . -B build
+cmake --build build
+```
+
+Executable will be `gen` in the `build/` directory.
+
+## Watch the Development
+
+YouTube video
+series "[Creating a Compiler](https://www.youtube.com/playlist?list=PLUDlas_Zy_qC7c5tCgTMYq2idyyT241qs)"
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,110 @@
-#include<iostream>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <optional>
+#include <vector>
 using namespace std;
 
-int main(int argc, char* argv[]){
+enum class TokenType
+{
+    _return,
+    int_lit,
+    semi
+};
 
-    if(argc != 2){
-        cerr<<"Incorrect usage. Correct usage is..."<<endl;
-        cerr<<"geny <filename>.gy"<<endl;
+struct Token
+{
+    TokenType type;
+    optional<string> value;
+};
+
+vector<Token> tokenize(const string &str)
+{
+    vector<Token> tokens;
+
+    string buff;
+
+    for (int i = 0; i < str.length(); i++)
+    {
+        char c = str[i];
+
+        if (isalpha(c))
+        {
+            buff.push_back(c);
+            i++;
+            while (isalnum(str.at(i)))
+            {
+                buff.push_back(str.at(i));
+                i++;
+            }
+            i--;
+
+            if (buff == "return")
+            {
+                tokens.push_back({.type = TokenType::_return});
+                buff.clear();
+                continue;
+            }
+            else
+            {
+                cerr << "You messed up" << endl;
+                exit(EXIT_FAILURE);
+            }
+        }
+        else if (isdigit(c))
+        {
+            buff.push_back(c);
+            i++;
+            while (isdigit(str.at(i)))
+            {
+                buff.push_back(str.at(i));
+                i++;
+            }
+            i--;
+
+            tokens.push_back({.type = TokenType::int_lit, .value = buff});
+            buff.clear();
+        }
+        else if (c == ';')
+        {
+            tokens.push_back({.type = TokenType::semi});
+        }
+
+        else if (isspace(c))
+        {
+            continue;
+        }
+        else
+        {
+            cerr << "You messed up" << endl;
+            exit(EXIT_FAILURE);
+        }
+    }
+    return tokens;
+}
+
+int main(int argc, char *argv[])
+{
+
+    if (argc != 2)
+    {
+        cerr << "Incorrect usage. Correct usage is..." << endl;
+        cerr << "geny <filename>.gn" << endl;
         return EXIT_FAILURE;
     }
 
-    cout<<"hello world"<<endl;
+    // reading from our source file
+    fstream input(argv[1], ios::in);
+    stringstream contents_stream;
+    contents_stream << input.rdbuf();
+    input.close();
+    string contents = contents_stream.str();
+    cout << contents << endl;
+
+    // lexical analysis
+    vector<Token> tokens = tokenize(contents);
+
+    cout << tokens.size();
+
     return EXIT_SUCCESS;
 }
