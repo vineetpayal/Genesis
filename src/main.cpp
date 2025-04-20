6e76a1a079d2207983e0d0c345ddcf97439eecc9
vineetpayal <vineet.pyl@gmail.com>
2025-04-20 12:30:41 +0530

    lexical analyser improved

--- src/main.cpp
+++ src/main.cpp
@@ -3,100 +3,22 @@
 #include <sstream>
 #include <optional>
 #include <vector>
+#include "tokenization.hpp"
 using namespace std;
 
-enum class TokenType
-{
-    exit,
-    int_lit,
-    semi
-};
 
-struct Token
-{
-    TokenType type;
-    optional<string> value;
-};
-
-vector<Token> tokenize(const string &str)
-{
-    vector<Token> tokens;
-
-    string buff;
-
-    for (int i = 0; i < str.length(); i++)
-    {
-        char c = str[i];
-
-        if (isalpha(c))
-        {
-            buff.push_back(c);
-            i++;
-            while (isalnum(str.at(i)))
-            {
-                buff.push_back(str.at(i));
-                i++;
-            }
-            i--;
-
-            if (buff == "exit")
-            {
-                tokens.push_back({.type = TokenType::exit});
-                buff.clear();
-                continue;
-            }
-            else
-            {
-                cerr << "You messed up" << endl;
-                exit(EXIT_FAILURE);
-            }
-        }
-        else if (isdigit(c))
-        {
-            buff.push_back(c);
-            i++;
-            while (isdigit(str.at(i)))
-            {
-                buff.push_back(str.at(i));
-                i++;
-            }
-            i--;
-
-            tokens.push_back({.type = TokenType::int_lit, .value = buff});
-            buff.clear();
-        }
-        else if (c == ';')
-        {
-            tokens.push_back({.type = TokenType::semi});
-        }
-
-        else if (isspace(c))
-        {
-            continue;
-        }
-        else
-        {
-            cerr << "You messed up" << endl;
-            exit(EXIT_FAILURE);
-        }
-    }
-    return tokens;
+vector<Token> tokenize(const string &str) {
 }
 
-string tokens_to_asm(vector<Token> &tokens)
-{
+string tokens_to_asm(vector<Token> &tokens) {
     stringstream output;
     output << "global _start\n_start:\n";
 
-    for (int i = 0; i < tokens.size(); i++)
-    {
+    for (int i = 0; i < tokens.size(); i++) {
         Token token = tokens[i];
-        if (token.type == TokenType::exit)
-        {
-            if (i + 1 < tokens.size() && tokens[i + 1].type == TokenType::int_lit)
-            {
-                if (i + 2 < tokens.size() && tokens[i + 2].type == TokenType::semi)
-                {
+        if (token.type == TokenType::exit) {
+            if (i + 1 < tokens.size() && tokens[i + 1].type == TokenType::int_lit) {
+                if (i + 2 < tokens.size() && tokens[i + 2].type == TokenType::semi) {
                     output << "    mov rax, 60\n";
                     output << "    mov rdi, " << tokens[i + 1].value.value() << "\n";
                     output << "    syscall";
@@ -107,11 +29,8 @@ string tokens_to_asm(vector<Token> &tokens)
     return output.str();
 }
 
-int main(int argc, char *argv[])
-{
-
-    if (argc != 2)
-    {
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
         cerr << "Incorrect usage. Correct usage is..." << endl;
         cerr << "geny <filename>.gn" << endl;
         return EXIT_FAILURE;
@@ -126,11 +45,10 @@ int main(int argc, char *argv[])
     cout << contents << endl;
 
     // lexical analysis
-    vector<Token> tokens = tokenize(contents);
-
-    {
+    Tokenizer tokenizer(move(contents));
+    vector<Token> tokens = tokenizer.tokenize(); {
         fstream file("../out.asm", ios::out);
-        file<<tokens_to_asm(tokens);
+        file << tokens_to_asm(tokens);
     }
 
     system("nasm -felf64 ../out.asm");
---
+++ src/tokenization.hpp
@@ -0,0 +1,87 @@
+#pragma once
+
+#include<iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+enum class TokenType {
+    exit,
+    int_lit,
+    semi
+};
+
+struct Token {
+    TokenType type;
+    optional<string> value;
+};
+
+class Tokenizer {
+public:
+    explicit inline Tokenizer(const string &src)
+        : m_src(move(src)) {
+    }
+
+    inline vector<Token> tokenize() {
+        vector<Token> tokens;
+
+        string buff;
+
+        while (peak().has_value()) {
+            if (isalpha(peak().value())) {
+                buff.push_back(consume());
+
+                while (peak().has_value() && isalnum(peak().value())) {
+                    buff.push_back(consume());
+                }
+
+                if (buff == "exit") {
+                    tokens.push_back({.type = TokenType::exit});
+                    buff.clear();
+                    continue;
+                } else {
+                    cerr << "You messed up!" << endl;
+                    exit(EXIT_FAILURE);
+                }
+            } else if (isdigit(peak().value())) {
+                buff.push_back(consume());
+                while (peak().has_value() && isdigit(peak().value())) {
+                    buff.push_back(consume());
+                }
+                tokens.push_back({.type = TokenType::int_lit, .value = buff});
+                buff.clear();
+                continue;
+            } else if (peak().value() == ';') {
+                consume();
+                tokens.push_back({.type = TokenType::semi});
+                continue;
+            } else if (isspace(peak().value())) {
+                consume();
+                continue;
+            } else {
+                cerr << "You messed up!" << endl;
+                exit(EXIT_FAILURE);
+            }
+        }
+        m_index = 0;
+        return tokens;
+    }
+
+private:
+    [[nodiscard]] optional<char> peak(int ahead = 1) const {
+        if (m_index + ahead > m_src.length()) {
+            return {};
+        } else {
+            return m_src.at(m_index);
+        }
+    }
+
+    char consume() {
+        return m_src.at(m_index++);
+    }
+
+    const string m_src;
+    int m_index = 0;
+};
