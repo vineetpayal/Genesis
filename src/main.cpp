6cd5a5d2817507ede5d37e573d5de3202b7c2712
vineetpayal <vineet.pyl@gmail.com>
2025-05-18 20:08:13 +0530

    implemented relational operators

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char* argv[])
 {
     if (argc != 2) {
         std::cerr << "Incorrect usage. Correct usage is..." << std::endl;
-        std::cerr << "hydro <input.hy>" << std::endl;
+        std::cerr << "geny <input.gn>" << std::endl;
         return EXIT_FAILURE;
     }
 
